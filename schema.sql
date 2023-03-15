f559b4368d9d2bbea42b9bf513427ee5b6154eb2
Matt Silverlock <msilverlock@cloudflare.com>
2023-03-15 10:19:29 -0400

    new client auth; add schema

---
+++ schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, first_name TEXT, last_name TEXT, car_make TEXT, car_model TEXT, car_vin TEXT);
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
-import { connect, Connection } from "./libsql-client";
+import { createClient, Client, ResultSet } from "@libsql/client/http";
 
 export interface Env {
   TURSO_DB_URL: string;
+  NUM_QUERIES: Number;
 }
 
 // Memoize our DB connection across instances
-let db: Connection;
+let db: Client;
 
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
-  ): Promise<Response> {
+  ): Promise<Response | void> {
     try {
+      let start = Date.now();
       if (db === undefined) {
-        db = connect({ url: env.TURSO_DB_URL });
+        db = createClient({ url: env.TURSO_DB_URL });
       }
+      console.log(`setup latency: ${Date.now() - start}ms`);
+
+      let numQueries = env.NUM_QUERIES ? env.NUM_QUERIES : 5;
 
       let now = Date.now();
+
+      // Generate some subrequests for Smart Placement
+      for (let i = 1; i <= numQueries; i++) {
+        console.log(`executing query #${i}...`);
+        await db.execute("SELECT * FROM users LIMIT 5;");
+        console.log(`query latency: ${Date.now() - now}ms`);
+      }
+
+      // Actual data query
       console.log(`executing query...`);
       let res = await db.execute("SELECT * FROM users;");
       console.log(`query latency: ${Date.now() - now}ms`);
